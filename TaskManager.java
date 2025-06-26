820ff997728eec02edd332f67870ddcd36dacf60
fizza-kashif <fizzakashif2025@gmail.com>
2025-06-25 23:16:49 -0700

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -1,26 +1,55 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.Arraylist;
-import java.util.Comparator;
 
 public class TaskManager {
+	private Task[] taskList = new Task[100]; 
+	private int size = 0; 
 
-    private final ArrayList<Task> taskList= new ArrayList<>();
+    	public void addTask(Task task) {
+    	    if (size < taskList.length) {
+    	        taskList[size] = task;
+    	        size++;
+    	    } else {
+    	        System.out.println("Task list is full!");
+    	    }
+    	}
 
-    public void addTask(Task task) {
-        taskList.add(task);
+
+public void removeTask(Task task) {
+    for (int i = 0; i < size; i++) {
+        if (taskList[i].equals(task)) {
+            for (int j = i; j < size - 1; j++) {
+                taskList[j] = taskList[j + 1];
+            }
+            taskList[size - 1] = null;
+            size--;
+            break;
+        }
     }
+}
 
-    public void removeTask(Task task) {
-        taskList.remove(task);
-    }
+public void sortByDueDate() {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            String date1 = taskList[j].getDueDate();
+            String date2 = taskList[j + 1].getDueDate();
 
-    public ArrayList<Task> getTasks() {
-        return taskList;
+            if (date1.compareTo(date2) > 0) {
+                Task temp = taskList[j];
+                taskList[j] = taskList[j + 1];
+                taskList[j + 1] = temp;
+            }
+        }
     }
+}
+public Task[] getTasks() {
+    Task[] currentTasks = new Task[size];
+    for (int i = 0; i < size; i++) {
+        currentTasks[i] = taskList[i];
+    }
+    return currentTasks;
+}
 
-    public void sortByDueDate() {
-        taskList.sort(Comparator.comparing(Task::getDueDate));
-    }
+
+    
 }
